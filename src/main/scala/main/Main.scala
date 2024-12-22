c32d439cd3a50764fdcaa25f539fb812aaf4d489
Gérard Lichtert <62557229+thebaconfate@users.noreply.github.com>
2024-12-22 05:28:10 +0100

    finished implementation

--- src/main/scala/main/Main.scala
+++ src/main/scala/main/Main.scala
@@ -4,22 +4,33 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
 object Main {
+  private type GMM = (Array[Double], Array[Double], Array[Double]);
+
+  def printResults(
+      arrays: (Array[Double], Array[Double], Array[Double])
+  ): Unit = {
+    println("Weights: " + arrays._1.mkString(", "))
+    println("Means: " + arrays._2.mkString(", "))
+    println("Variances " + arrays._3.mkString(", "))
+  }
+
   def main(args: Array[String]): Unit = {
     val conf: SparkConf = new SparkConf()
     conf.setAppName("Datasets Test")
     conf.setMaster("local[*]")
     val filename = "dataset.txt"
     val sc = new SparkContext(conf)
+    val epsilon = 0.001
     val initRdd = sc.textFile(filename).map(_.toDouble).persist()
-    EM(initRdd, 5)
-    EM(initRdd, 10)
+    printResults(EM(initRdd, 3, epsilon))
+    //printResults(EM(initRdd, 10, epsilon))
 
   }
 
   private def logLikelihood(
       X: RDD[Double],
       weights: Array[Double],
-      sample: Array[Double],
+      means: Array[Double],
       variance: Array[Double]
   ): Double = {
     val K = weights.length
@@ -28,7 +39,7 @@ object Main {
         (0 until K)
           .map((k) =>
             weights(k) * Math.exp(
-              -0.5 * Math.pow(x - sample(k), 2) / variance(k)
+              -0.5 * Math.pow(x - means(k), 2) / variance(k)
             ) / Math.sqrt(2 * Math.PI * variance(k))
           )
           .sum
@@ -54,15 +65,15 @@ object Main {
     })
   }
 
-  private def EM(X: RDD[Double], K: Int): Unit = {
+  private def EM(X: RDD[Double], K: Int, epsilon: Double): GMM = {
     var meansVector = X.takeSample(withReplacement = false, num = K)
     val count = X.count()
     val variance = X.variance()
     var varianceVector = Array.fill(K)(variance)
-    var weightsVector: Array[Double] = Array.fill(K)(1 / K)
-    val currentLogLikelihood =
+    var weightsVector: Array[Double] = Array.fill(K)(1.0 / K)
+    var currentLogLikelihood =
       logLikelihood(X, weightsVector, meansVector, varianceVector)
-    val prevLogLikelihood = currentLogLikelihood
+    var prevLogLikelihood: Double = currentLogLikelihood
 
     do {
       val gammaRDD = gamma(
@@ -71,12 +82,10 @@ object Main {
         means = meansVector,
         variances = varianceVector
       ).persist()
-      val sumGammas = gammaRDD.reduce((prev, current) =>
+      val sumGammas: Array[Double] = gammaRDD.reduce((prev, current) =>
         prev
           .zip(current)
-          .map((prevZippedCurrent) =>
-            prevZippedCurrent._1 + prevZippedCurrent._2
-          )
+          .map(prevZippedCurrent => prevZippedCurrent._1 + prevZippedCurrent._2)
       )
       weightsVector = sumGammas.map(_ / count)
       meansVector = gammaRDD
@@ -94,16 +103,29 @@ object Main {
           sumGammaTimesMeanZippedSumGammas._1 / sumGammaTimesMeanZippedSumGammas._2
         )
       varianceVector = gammaRDD
-        .zip(X.map(x => varianceVector.map(v => Math.pow(x - v, 2))))
-        .map(zipped => zipped._1.zip(zipped._2).map(t => t._1 * t._2))
+        .zip(X.map(x => meansVector.map(mean => Math.pow(x - mean, 2))))
+        .map(gammaZippedXMinusMeansSquared =>
+          gammaZippedXMinusMeansSquared._1
+            .zip(gammaZippedXMinusMeansSquared._2)
+            .map(gammaZippedXMinusMeansSquaredVectors =>
+              gammaZippedXMinusMeansSquaredVectors._1 * gammaZippedXMinusMeansSquaredVectors._2
+            )
+        )
         .reduce((a, b) => a.zip(b).map(t => t._1 + t._2))
         .zip(sumGammas)
         .map(t => t._1 / t._2)
 
-    } while (false)
-    // println(s"Means: " + meansVector.toString())
-    // println(s"Variance: " + varianceVector.toString())
-    // println(s"Weights: " + weightsVector.toString())
-    println("logLikelihood: " + currentLogLikelihood)
+      prevLogLikelihood = currentLogLikelihood
+      println("Intermediate results: ")
+      printResults((weightsVector, meansVector, varianceVector))
+      currentLogLikelihood = logLikelihood(
+        X,
+        weights = weightsVector,
+        means = meansVector,
+        variance = varianceVector
+      )
+
+    } while ((currentLogLikelihood - prevLogLikelihood) > epsilon)
+    (weightsVector, meansVector, varianceVector)
   }
 }
